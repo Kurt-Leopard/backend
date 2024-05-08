c9cf9b455325e407f1645a5315a12d4197f2073a
Kurt Leopard <markangeloujamandre@gmail.com>
2024-05-09 00:29:35 +0800

    update env

--- index.mjs
+++ index.mjs
@@ -11,9 +11,15 @@ dotenv.config();
 
 const app = express();
 const port=process.env.SERVER_PORT;
+const corsConfig={
+    origin:"*",
+    credential:true,
+    methods:["GET","POST","PUT","DELETE"]
 
+}
 app.use(express.json());
-app.use(cors());
+app.options("",cors(corsConfig));
+app.use(cors(corsConfig));
 app.use(userRouter);
 app.use(loginRouter);
 app.use(authenticatedUserRouter);
